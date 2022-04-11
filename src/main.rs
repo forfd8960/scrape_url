ec1b86aefe13eb6bad1bbadcbd75927f4a3bb0d2
forfd8960 <forfd8960@gmail.com>
2022-04-11 20:47:23 +0800

    broadcast error to Result<T, E>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::fs;
 
-fn main() {
+fn main() -> Result<(), Box <dyn std::error::Error>>{
     println!("Hello, world!");
 
     let url = "https://www.rust-lang.org/";
     let output = "rust.md";
 
     println!("Fetching url: {}", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let body = reqwest::blocking::get(url)?.text()?;
 
     println!("converting html to markdown...");
     let md = html2md::parse_html(&body);
 
-    fs::write(output, md.as_bytes()).unwrap();
+    fs::write(output, md.as_bytes())?;
     println!("converted markdown has been saved in {}.", output);
+
+    Ok(())
 }
